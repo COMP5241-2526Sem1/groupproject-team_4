3069d85838191b8c5488ed8fd9b199ea4cb42a45
25065285G <25065285g@connect.polyu.hk>
2025-11-17 23:57:41 +0800

    chore: clean

--- test_pictures.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from database import db
-from models.picture import Picture
-
-with app.app_context():
-    pictures = Picture.query.all()
-    print(f'Total pictures: {len(pictures)}')
-    for p in pictures:
-        print(f'ID: {p.id}, Minigame: {p.minigame_id}, User: {p.user_id}, Type: {p.file_type}, Size: {p.file_size}')
